f5fe689dcdc7008d9d04ceea53321d2b019c0f48
Toshiaki Mitsukura <mitsukura@gmail.com>
2025-02-17 13:58:32 +0900

    fix: 文字修正

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <h2>四季守</h2>
         <ul className="flex gap-4 list-none">
           <li>重機除雪</li>
-          <li>マシン草刈り</li>
+          <li>重機草刈り（準備中）</li>
           <li>お問い合わせ</li>
         </ul>
         <p>&copy; 2025 Shikimori All Rights Reserved.</p>
