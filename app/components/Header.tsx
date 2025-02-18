fdaa431596bb07a739a0c38e23f69d8eff13b614
Toshiaki Mitsukura <mitsukura@gmail.com>
2025-02-19 04:25:56 +0900

    feat: Aboutページ作成

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,16 +1,20 @@
+import Link from 'next/link'
+
 export default function Header() {
   return (
-    <header className='flex h-16 items-center justify-between border-b border-slate-200 dark:border-slate-700 px-6'>
-    <h1 className='text-md font-bold tracking-wide'>四季守</h1>
-    <nav className='flex gap-4 text-sm'>
-      <ul className="flex gap-4 list-none">
-        <li>四季守とは</li>
-        <li>メニュー</li>
-        <li>お問い合わせ</li>
-        <li>登録</li>
-        <li>ログイン</li>
-      </ul>
-    </nav>
-  </header>
+    <header className='flex h-16 items-center justify-between border-b border-slate-200 px-6 dark:border-slate-700'>
+      <h1 className='text-md font-bold tracking-wide'>四季守</h1>
+      <nav className='flex gap-4 text-sm'>
+        <ul className='flex list-none gap-4'>
+          <li>
+            <Link href='/about'>四季守とは</Link>
+          </li>
+          <li>メニュー</li>
+          <li>お問い合わせ</li>
+          <li>登録</li>
+          <li>ログイン</li>
+        </ul>
+      </nav>
+    </header>
   )
 }
