1fb595e521c04cdd1e905ae2e0ce2035ae049f6b
Toshiaki Mitsukura <mitsukura@gmail.com>
2025-02-19 16:25:21 +0900

    fix: cssの修正

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <footer className='sticky top-full h-16 items-center border-t border-slate-200 dark:border-slate-700 p-6 text-sm'>
+    <footer className='sticky top-full items-center border-t p-6 text-sm'>
       <div className='flex flex-col items-center justify-center gap-2 pb-8'>
         <h2>四季守</h2>
-        <ul className="flex gap-4 list-none">
+        <ul className='flex list-none gap-4'>
           <li>重機除雪</li>
           <li>重機草刈り（準備中）</li>
           <li>お問い合わせ</li>
