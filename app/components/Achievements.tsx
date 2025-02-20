3f961e2774519d11262d16bae7dcbf295ea7d739
Toshiaki Mitsukura <mitsukura@gmail.com>
2025-02-20 20:14:44 +0900

    feat: プライバシー・ポリシーページの作成

--- app/components/Achievements.tsx
+++ app/components/Achievements.tsx
@@ -9,28 +9,27 @@ export default function Achievements() {
     <>
       <div className='flex flex-col gap-4'>
         <h2>実績</h2>
-      <div className='flex gap-4'>
-        {achievements.map((achievement: Achievement) => (
-          <div
-            key={achievement.id}
-            className='rounded border-2 border-gray-200 bg-muted p-4'
-          >
-            <div className='aspect-video rounded-sm'>
-              <Image
-                src={achievement.imageSrc}
-                alt={achievement.alt}
-                width={240}
-                height={240}
-                className='rounded-sm' 
-              />
+        <div className='flex gap-4'>
+          {achievements.map((achievement: Achievement) => (
+            <div
+              key={achievement.id}
+              className='rounded border-2 border-gray-200 bg-muted p-4'
+            >
+              <div className='relative aspect-video w-60 rounded-sm border-2 border-muted'>
+                <Image
+                  src={achievement.imageSrc}
+                  alt={achievement.alt}
+                  fill
+                  className='rounded-sm object-cover object-top'
+                />
+              </div>
+              <div className='my-2 text-sm font-semibold'>
+                {achievement.title}
+              </div>
+              <div className='font-mono text-xs'>{achievement.date}</div>
             </div>
-            <div className='my-2 text-sm font-semibold'>
-              {achievement.title}
-            </div>
-            <div className='font-mono text-xs'>{achievement.date}</div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       </div>
     </>
   )
