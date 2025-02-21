6c27cdccfb9c212b00bc6f8755c60b2b1d53314b
mitsukura <mitsukura@gmail.com>
2025-02-21 12:56:53 +0900

    feat: お問い合わせページ（google form) (#15)

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <footer className='sticky top-full items-center border-t p-6 text-sm'>
-      <div className='flex flex-col items-center justify-center gap-2 pb-8'>
+      <div className='flex flex-col items-center justify-center gap-2 pb-8 text-muted-foreground'>
         <h2>四季守</h2>
         <ul className='flex list-none gap-4'>
           {footerNavListItems.map(item => (
