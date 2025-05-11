ab883908715dc28dcd5046ca80b74a2408fa0e14
mitsukura <mitsukura@gmail.com>
2025-05-11 12:47:57 +0900

    feat: 非認証へッダーにプロフィールリンクを非表示にし、Voltaの設定を追加 (#62)

--- data/navigations.ts
+++ data/navigations.ts
@@ -2,7 +2,6 @@ export const navListItems = [
   { label: '四季守とは', href: '/about' },
   { label: 'メニュー', href: '/menu' },
   { label: 'お問い合わせ', href: '/contact' },
-  { label: 'プロフィール', href: '/profile' },
 ]
 
 export const footerNavListItems = [
